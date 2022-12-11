8e347e81186e1b9865eeff39594e7ee4fca7334d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-12-11 12:26:29 -0300

    Revert base url for wagtail

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -185,7 +185,7 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-WAGTAILADMIN_BASE_URL = "https://falconhunt.xyz/blog"
+WAGTAILADMIN_BASE_URL = "https://falconhunt.xyz"
 
 CSRF_TRUSTED_ORIGINS = ["https://*.falconhunt.xyz", "https://*.127.0.0.1"]
 
