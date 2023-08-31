90e78a15d10046759a0719a535dc39d17c5a4866
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-08-31 16:56:57 -0300

    Add partner model with list and detail views

--- mysite/partners/models.py
+++ mysite/partners/models.py
@@ -1,19 +1,19 @@
 import logging
 
 from django import forms
+from django.core.paginator import Paginator
 from django.db import models
 
-from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
-from wagtail.blocks import ListBlock
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.fields import StreamField
 from wagtail.models import Page
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
 
-from base.blocks import BaseStreamBlock, ImageBlock
+from base.blocks import BaseStreamBlock
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from modelcluster.models import ParentalKey, ParentalManyToManyField
-from taggit.models import Tag, TaggedItemBase
+from taggit.models import TaggedItemBase
 
 logger = logging.getLogger(__name__)
 
@@ -30,6 +30,48 @@ class PartnerPageTag(TaggedItemBase):
     content_object = ParentalKey(
         "PartnerPage", related_name="tagged_items", on_delete=models.CASCADE
     )
+
+
+class PartnerIndexPage(Page):
+    page_description = "Use this page to show a list of partners"
+
+    intro = models.TextField(help_text="Text to describe the page", blank=True)
+
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Landscape mode only; horizontal width between 1000px to 3000px.",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+        FieldPanel("image"),
+    ]
+
+    # Specify that only blog pages can be children of this blogindex page
+    subpage_types = ["PartnerPage"]
+
+    class Meta:
+        verbose_name = "partnerindexpage"
+        verbose_name_plural = "partnerindexpages"
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        qs = (
+            self.get_children().live().order_by("-first_published_at")
+        )  # <-- change this to by country
+
+        paginator = Paginator(qs, 9)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
+        context["partners"] = context["page_obj"] = page_obj
+
+        return context
 
 
 class PartnerPage(Page):
@@ -70,12 +112,26 @@ class PartnerPage(Page):
             heading="Partner Information",
         ),
     ]
-    # parent_page_types = ["partners.PartnerIndexPage"]
-    # subpage_types = []
+    parent_page_types = ["partners.PartnerIndexPage"]
+    subpage_types = []
 
     class Meta:
         verbose_name = "partnerpage"
         verbose_name_plural = "partnerpages"
+
+    def get_tags(self):
+        """
+        Find all the tags that are related to the blog post into a list we can access on the template.
+        We're additionally adding a URL to access BlogPage objects with that tag
+        """
+
+        base_url = self.get_parent().url
+        tags = self.tags.all()
+
+        for tag in tags:
+            tag.url = f"{base_url}tags/{tag.slug}/"
+
+        return tags
 
 
 @register_snippet
