ef1dfcdc1f19c031ad808793b709639e9bfb2ff7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2024-05-07 16:30:34 -0300

    Add basic unit tests for base models

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -10,7 +10,7 @@ from blog.models import BlogIndexPage, BlogPage
 logger = logging.getLogger(__name__)
 
 
-class BlogIndexPageRoutabilityTests(WagtailPageTestCase):
+class BlogIndexPageTests(WagtailPageTestCase):
     """
     Test suite to check if the blog index page is routable under different routes.
     """
@@ -18,14 +18,12 @@ class BlogIndexPageRoutabilityTests(WagtailPageTestCase):
     @classmethod
     def setUpTestData(cls):
         try:
-            default_home = Page.objects.filter(
-                title="Welcome to your new Wagtail site!"
-            )[0]
+            default_home = Page.objects.get(title="Welcome to your new Wagtail site!")
             default_home.slug = "home-old"
             default_home.save_revision().publish()
             default_home.save()
 
-        except:  # noqa
+        except Page.DoesNotExist:
             pass
 
         cls.root = Page.objects.get(id=1).specific
@@ -33,6 +31,7 @@ class BlogIndexPageRoutabilityTests(WagtailPageTestCase):
             title="Home", slug="home", hero_text="You can do it", hero_cta="Learn More"
         )
         cls.blog_index_page = BlogIndexPage(title="blog", slug="blog")
+        cls.blog_page = BlogPage(title="Buenos Aires", slug="buenos-aires")
 
         # Set Home Page as child of root
         cls.root.add_child(instance=cls.home_page)
@@ -51,6 +50,17 @@ class BlogIndexPageRoutabilityTests(WagtailPageTestCase):
         cls.blog_index_page.save_revision().publish()
         cls.blog_index_page.save()
 
+        # Add BlogPage as child of BlogIndex
+        # Veer you need a blog page as the blog index has a featured blog post section
+        # so atleast one blog page is needed. Temp fix for now
+        cls.blog_index_page.add_child(instance=cls.blog_page)
+        cls.blog_page.save_revision().publish()
+        cls.blog_page.save()
+
+    def test_get(self):
+        response = self.client.get(self.blog_index_page.url)
+        self.assertEqual(response.status_code, 200)
+
     def test_default_route(self):
         self.assertPageIsRoutable(self.blog_index_page)
 
@@ -59,10 +69,6 @@ class BlogIndexPageRoutabilityTests(WagtailPageTestCase):
 
     def test_tags_specific_route(self):
         self.assertPageIsRoutable(self.blog_index_page, "tags/bus/")
-
-    # Not sure why this test doesn't pass!
-    # def test_default_route_rendering(self):
-    #     self.assertPageIsRenderable(page=self.blog_index_page, route_path="/")
 
     def test_editability(self):
         self.assertPageIsEditable(self.blog_index_page)
@@ -86,3 +92,54 @@ class BlogIndexPageRoutabilityTests(WagtailPageTestCase):
 
     def test_blog_index_page_subpages(self):
         self.assertAllowedSubpageTypes(BlogIndexPage, {BlogPage})
+
+
+class BlogPageTests(WagtailPageTestCase):
+    """
+    Test suite for the blog post page.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        try:
+            default_home = Page.objects.get(title="Welcome to your new Wagtail site!")
+            default_home.slug = "home-old"
+            default_home.save_revision().publish()
+            default_home.save()
+
+        except Page.DoesNotExist:
+            pass
+
+        cls.root = Page.objects.get(id=1).specific
+        cls.home_page = HomePage(
+            title="Home", slug="home", hero_text="You can do it", hero_cta="Learn More"
+        )
+        cls.blog_index_page = BlogIndexPage(title="blog", slug="blog")
+        cls.blog_page = BlogPage(title="Buenos Aires", slug="buenos-aires")
+
+        # Set Home Page as child of root
+        cls.root.add_child(instance=cls.home_page)
+        cls.home_page.save_revision().publish()
+        cls.home_page.save()
+
+        # Set default Home Page as root page for Site
+        cls.site = Site.objects.get(id=1)
+        cls.site.root_page = cls.home_page
+        cls.site.save()
+
+        # Add BlogIndex as child of Home Page
+        cls.home_page.add_child(instance=cls.blog_index_page)
+        cls.blog_index_page.save_revision().publish()
+        cls.blog_index_page.save()
+
+        # Add BlogPage as child of BlogIndex
+        cls.blog_index_page.add_child(instance=cls.blog_page)
+        cls.blog_page.save_revision().publish()
+        cls.blog_page.save()
+
+    def test_get(self):
+        response = self.client.get(self.blog_page.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_default_route(self):
+        self.assertPageIsRoutable(self.blog_page)
--- mysite/locations/tests.py
+++ mysite/locations/tests.py
@@ -1,15 +1,16 @@
 import logging
 
 from wagtail.models import Page, Site
+from wagtail.rich_text import RichText
 from wagtail.test.utils import WagtailPageTestCase
 
 from home.models import HomePage
-from locations.models import CityIndexPage, CityPage
+from locations.models import CityIndexPage, CityPage, StationPage
 
 logger = logging.getLogger(__name__)
 
 
-class CityIndexPageRoutabilityTests(WagtailPageTestCase):
+class CityIndexPageTests(WagtailPageTestCase):
     """
     Test suite for the city index page
     """
@@ -17,14 +18,12 @@ class CityIndexPageRoutabilityTests(WagtailPageTestCase):
     @classmethod
     def setUpTestData(cls):
         try:
-            default_home = Page.objects.filter(
-                title="Welcome to your new Wagtail site!"
-            )[0]
+            default_home = Page.objects.get(title="Welcome to your new Wagtail site!")
             default_home.slug = "home-old"
             default_home.save_revision().publish()
             default_home.save()
 
-        except:  # noqa
+        except Page.DoesNotExist:
             pass
 
         cls.root = Page.objects.get(id=1).specific
@@ -50,6 +49,10 @@ class CityIndexPageRoutabilityTests(WagtailPageTestCase):
         cls.city_index_page.save_revision().publish()
         cls.city_index_page.save()
 
+    def test_get(self):
+        response = self.client.get(self.city_index_page.url)
+        self.assertEqual(response.status_code, 200)
+
     def test_default_route(self):
         self.assertPageIsRoutable(self.city_index_page)
 
@@ -66,8 +69,165 @@ class CityIndexPageRoutabilityTests(WagtailPageTestCase):
         self.assertCanNotCreateAt(CityIndexPage, HomePage)
         self.assertCanNotCreateAt(CityPage, CityIndexPage)
 
-    def test_city_index_page_parent_pages(self):
-        self.assertAllowedParentPageTypes(CityIndexPage, {HomePage})
-
     def test_city_index_page_subpages(self):
         self.assertAllowedSubpageTypes(CityIndexPage, {CityPage})
+
+
+class CityPageTests(WagtailPageTestCase):
+    """
+    Test suite for City Page.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        try:
+            default_home = Page.objects.get(title="Welcome to your new Wagtail site!")
+            default_home.slug = "home-old"
+            default_home.save_revision().publish()
+            default_home.save()
+
+        except Page.DoesNotExist:
+            pass
+
+        cls.root = Page.objects.get(id=1).specific
+        cls.home_page = HomePage(
+            title="Home", slug="home", hero_text="You can do it", hero_cta="Learn More"
+        )
+        cls.city_index_page = CityIndexPage(title="cities", slug="cities")
+        cls.city_page = CityPage(title="Buenos Aires", slug="buenos-aires")
+
+        # Set Home Page as child of root
+        cls.root.add_child(instance=cls.home_page)
+
+        # Save and publish Home Page
+        cls.home_page.save_revision().publish()
+        cls.home_page.save()
+
+        # Set default Home Page as root page for Site
+        cls.site = Site.objects.get(id=1)
+        cls.site.root_page = cls.home_page
+        cls.site.save()
+
+        # Add CityIndexPage as child of HomePage
+        cls.home_page.add_child(instance=cls.city_index_page)
+        cls.city_index_page.save_revision().publish()
+        cls.city_index_page.save()
+
+        # Add CityPage as child of CityIndexPage
+        cls.city_index_page.add_child(instance=cls.city_page)
+        cls.city_page.save_revision().publish()
+        cls.city_page.save()
+
+    def test_get(self):
+        response = self.client.get(self.city_page.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_default_route(self):
+        self.assertPageIsRoutable(self.city_page)
+
+    def test_editability(self):
+        self.assertPageIsEditable(self.city_page)
+
+    def test_can_create_city_page_under_city_index(self):
+        self.assertCanCreateAt(CityIndexPage, CityPage)
+
+    def test_can_create_station_page_under_citypage(self):
+        self.assertCanCreateAt(CityPage, StationPage)
+
+    def test_cannot_create_wrong_children_or_parents_for_city_page(self):
+        self.assertCanNotCreateAt(CityPage, HomePage)
+        self.assertCanNotCreateAt(StationPage, CityPage)
+
+    def test_city_page_subpages(self):
+        self.assertAllowedSubpageTypes(CityPage, {StationPage})
+
+
+class StationPageTests(WagtailPageTestCase):
+    """
+    Test suite for Station Page.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        try:
+            default_home = Page.objects.get(title="Welcome to your new Wagtail site!")
+            default_home.slug = "home-old"
+            default_home.save_revision().publish()
+            default_home.save()
+
+        except Page.DoesNotExist:
+            pass
+
+        cls.root = Page.objects.get(id=1).specific
+        cls.home_page = HomePage(
+            title="Home", slug="home", hero_text="You can do it", hero_cta="Learn More"
+        )
+        cls.city_index_page = CityIndexPage(title="cities", slug="cities")
+        cls.city_page = CityPage(title="Buenos Aires", slug="buenos-aires")
+        cls.station_page = StationPage(
+            title="Terminal Omnibus",
+            slug="terminal-omnibus",
+            address="Av. Gdor. Ricardo Videla Mendoza Argentina",
+            lat_long="-32.89481666936962, -68.829083231125",
+        )
+        cls.station_page.body.extend(
+            [
+                ("heading_block", "Just a CharField Heading"),
+                ("paragraph_block", RichText("<p>First paragraph</p>")),
+                ("paragraph_block", RichText("<p>Second paragraph</p>")),
+            ]
+        )
+
+        # Set Home Page as child of root
+        cls.root.add_child(instance=cls.home_page)
+        cls.home_page.save_revision().publish()
+        cls.home_page.save()
+
+        # Set default Home Page as root page for Site
+        cls.site = Site.objects.get(id=1)
+        cls.site.root_page = cls.home_page
+        cls.site.save()
+
+        # Add CityIndexPage as child of HomePage
+        cls.home_page.add_child(instance=cls.city_index_page)
+        cls.city_index_page.save_revision().publish()
+        cls.city_index_page.save()
+
+        # Add CityPage as child of CityIndexPage
+        cls.city_index_page.add_child(instance=cls.city_page)
+        cls.city_page.save_revision().publish()
+        cls.city_page.save()
+
+        # Add StationPage as child of CityPage
+        cls.city_page.add_child(instance=cls.station_page)
+        cls.station_page.save_revision().publish()
+        cls.station_page.save()
+
+    def test_get(self):
+        response = self.client.get(self.station_page.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Just a CharField Heading")
+        self.assertContains(response, "<p>First paragraph</p>")
+        self.assertContains(response, "<p>Second paragraph</p>")
+
+    def test_default_route(self):
+        self.assertPageIsRoutable(self.station_page)
+
+    # def test_editability(self):
+    #     self.assertPageIsEditable(self.station_page)
+
+    # def test_general_previewability(self):
+    #     self.assertPageIsPreviewable(self.station_page)
+
+    def test_can_create_station_page_under_city_page(self):
+        self.assertCanCreateAt(CityPage, StationPage)
+
+    def test_can_create_city_page_under_stationpage(self):
+        self.assertCanNotCreateAt(StationPage, CityPage)
+
+    def test_cannot_create_wrong_children_or_parents_for_station_page(self):
+        self.assertCanNotCreateAt(HomePage, StationPage)
+        self.assertCanNotCreateAt(StationPage, CityPage)
+
+    def test_station_page_subpages(self):
+        self.assertAllowedSubpageTypes(StationPage, {})
