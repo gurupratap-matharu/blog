7caf916cd82ee17adbbb711898e4e7d81616f567
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2025-01-27 15:44:06 -0300

    Rename search slug to buscar

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -43,7 +43,7 @@ urlpatterns += i18n_patterns(
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path("search/", search_views.search, name="search"),
+    path("buscar/", search_views.search, name="search"),
     path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
